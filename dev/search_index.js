55ff99f594c9cba6897448ba697f8da26ad3a67a
Documenter.jl <documenter@juliadocs.github.io>
2023-05-23 09:53:59 +0000

    build based on cfe213e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RootsHelper","category":"page"},{"location":"#RootsHelper","page":"Home","title":"RootsHelper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RootsHelper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RootsHelper]","category":"page"}]
+}
